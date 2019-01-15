d4798b4eb55b9df1c645a1793efe3768f4430b5f
Iroh Sycorax <46717837+Uncle-Iroh@users.noreply.github.com>
2019-01-15 18:59:10 +0000

    Update mute.js

--- mute.js
+++ mute.js
@@ -1,16 +1,15 @@
-    if (comando === 'mute') {
-
+if (comando === 'mute') {
 
     let membro = message.mentions.members.first();
     if (!membro) {
-         message.reply('Por favor, mencione um usuário!');
+         return message.reply('Por favor, mencione um usuário!');
+    }
+
+    if (!message.member.hasPermission('BAN_MEMBERS')) {
+         return message.reply('Você não tem autorização!');
     }
 
     const cargoMutado = message.guild.roles.find(cargo => cargo.name === 'Mutado');
-    if (!message.member.hasPermission('BAN_MEMBERS')) {
-         message.reply('Você não tem autorização!');
-    }
-
 
     let motivo = args.slice(1).join(" ");
     if (!motivo) {
@@ -19,9 +18,7 @@
 
     const canal = client.channels.get('ID DO CANAL');
 
-
     let mutadoEmbed = new Discord.RichEmbed()
-
         .setTitle("💥🔨 Usário mutado do servidor")
         .setColor("#0000ff")
         .setThumbnail(membro.user.avatarURL)
@@ -33,21 +30,23 @@
         .setFooter(client.user.name, client.user.displayAvatarURL)
         .setTimestamp();
 
-        let PvEmbed = new Discord.RichEmbed()
-
+    let PvEmbed = new Discord.RichEmbed()
         .setTitle('-------------⚠ AVISO ⚠-------------')
         .setAuthor(message.guild.name)
         .setColor("#ff0000")
         .setThumbnail(message.guild.iconURL)
         .setDescription('Você foi **__mutado__** em nosso servidor🤐')
         .addField("`📃 Motivo:`", motivo)
-        .addField('Esperamos que compreenda o motivo.' ,'Sinta-se à vontade para contactar o responsável.')
+        .addField('Esperamos que compreenda o motivo.', 'Sinta-se à vontade para contactar o responsável.')
         .addField("`👮 Responsável:`", message.author)
         .setFooter(client.user.name, client.user.displayAvatarURL)
         .setImage('https://cdn.discordapp.com/attachments/533800753752571915/534625712460529684/PicsArt_01-15-04.51.34.png')
         .setTimestamp();
 
-    membro.addRole(cargoMutado);
-    membro.send(PvEmbed);
-    canal.send(mutadoEmbed);
+    membro.addRole(cargoMutado)
+        .then(() => {
+            membro.send(PvEmbed);
+            canal.send(mutadoEmbed);
+        })
+        .catch(() => message.reply("Não foi possível mutar o usuário"));
 }
